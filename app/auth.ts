5dd5d0404a789cfb1995915bb8bfe9c119d442e4
IraSidorenk0 <irasid12345@gmail.com>
2026-01-13 21:06:08 +0200

    Fix auth error

--- app/auth.ts
+++ app/auth.ts
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-// auth.ts
-import NextAuth from 'next-auth';
-import { authOptions } from './auth.config';
-
-const handler = NextAuth(authOptions);
-
-export const auth = handler.auth;
-export const GET = handler.GET;
-export const POST = handler.POST;
-export const signIn = handler.signIn;
-export const signOut = handler.signOut;
-=======
 import { NextAuthOptions } from 'next-auth';
 import { FirestoreAdapter } from '@auth/firebase-adapter';
 import { cert } from 'firebase-admin/app';
@@ -54,4 +41,3 @@ declare module 'next-auth' {
     };
   }
 }
->>>>>>> feature/ServerMethods
--- app/auth/auth-session.ts
+++ app/auth/auth-session.ts
@@ -1,7 +1,8 @@
 // auth-session.ts
-import { auth } from '../auth';
+import { getServerSession as nextGetServerSession } from 'next-auth';
+import { authOptions } from '../auth';
 
 export async function getServerSession() {
-  const session = await auth();
+  const session = await nextGetServerSession(authOptions);
   return session;
 }
